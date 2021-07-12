5e13bd5748b8d7b80baa04c7020f1edde3d20e5b
Dragifey <sahniquawut@gmail.com>
2021-07-12 11:58:29 +0200

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,8 +86,75 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
+/*==================== ANIMATIONS ====================*/ 
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: '2600',
+    // reset: true,
+})
+
+sr.reveal(`.home__data, .home__img, .resource__data, .footer__data, .section__heading`,{
+    origin: 'top',
+    interval: 50,
+})
+
+sr.reveal(`.about__container-image.animation-right, .about__container-data.animation-right`,{
+    origin: 'left',
+})
+
+sr.reveal(`.about__container-image.anination-left, .show-on-mobile, .about__container-data.animation-left`,{
+    origin: 'right',
+})
+
+sr.reveal(`.resources`,{
+    origin: 'top'
+})
+
+sr.reveal(`.category-filter`,{
+    origin: 'top',
+    delay: 300
+})
+
+sr.reveal(`.card-item`,{
+    origin: 'top',
+    interval: 100,
+    delay: 50
+})
 
 
+
+$(".bottom-line").css({
+    width : $(".widget__tabs .widget__nav a").first().width() + 20 + "px" // 20 = element's padding * 2
+});
+var _current_index = 0;
+$(".widget__tabs .widget__nav a").click(function(e){
+    e.preventDefault();
+    // tab navigation styles
+    var _this = $(this);
+    var _index = _this.index();
+    if(_current_index !== _index){
+        $(".tab nav a").each(function(){
+            if($(this).hasClass("current")) $(this).removeClass("current");
+        });
+        _this.addClass("current");
+        $(".bottom-line").css({
+            left : _this.offset().left - _this.parent().offset().left + "px",
+            width : _this.width() + 20 + "px" // 20 = element's padding * 2
+        });
+
+        // show tab content
+        $(".tab_single.shown").fadeOut(200);
+        setTimeout(function(){
+            $(".tab_single").eq(_index).fadeIn(200);
+            $(".tab_single").eq(_index).addClass("shown");
+        },200);
+
+        _current_index = _index;
+    }
+});
+
+/*==================== RESOURCES // CARDS FILTER ====================*/ 
 
 const filterContainer = document.querySelector(".category-filter"),
 galleryItems = document.querySelectorAll(".card-item");
